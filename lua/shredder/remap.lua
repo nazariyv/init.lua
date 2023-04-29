489721a85649bcc97be3d60579e6a6f16f352ce8
naz <hi@naz.ooo>
2023-04-29 01:28:28 +0100

    feat: remap to write all and close windows

--- lua/shredder/remap.lua
+++ lua/shredder/remap.lua
@@ -36,14 +36,14 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- this keymap ensures that subsequent hits
 -- of `p` actually paste foo instead
 -- greatest remap ever according to the primeagen
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", '\"_dP')
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", '\"+y')
+vim.keymap.set("n", "<leader>Y", '\"+Y')
 
 -- deleting into void register (doesn't get saved in buffer)
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", '\"_d')
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -61,6 +61,9 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- write and close all windows
+vim.keymap.set('n', '<leader>qq', ':wqa<CR>')
+
 -- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
@@ -68,4 +71,3 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 -- vim.keymap.set("n", "<leader><leader>", function()
 --     vim.cmd("so")
 -- end)
-
