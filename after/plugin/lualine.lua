ed65f12dbfe3f1f73c591ff1d5b5e545ceb6e636
naz <hi@naz.ooo>
2023-04-29 01:43:39 +0100

    feat(lualine): relative file path and icons

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,8 +2,9 @@
 -- See `:help lualine.txt`
 require('lualine').setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     component_separators = '|',
     section_separators = '',
+    path = 1,
   },
 }
